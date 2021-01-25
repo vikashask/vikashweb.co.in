5771bf06143c175e2fb451cf413e92c7cd3bad5a
vikash <vikashraj144@gmail.com>
2021-01-25 15:15:08 +0530

    skill  added

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,6 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import Fade from 'react-reveal/Fade';
+import { Container, Table } from 'react-bootstrap';
 import PortfolioContext from '../../context/context';
 import Title from '../Title/Title';
 
@@ -22,47 +21,29 @@ const Skills = () => {
   return (
     <section id="projects">
       <Container>
-        <div className="project-wrapper">
-          <Title title="Projects" />
-          {skill.map((skillData) => {
-            const { name, id, version, experience } = skillData;
-            return (
-              <Row key={id}>
-                <Col lg={4} sm={12}>
-                  <Fade
-                    left={isDesktop}
-                    bottom={isMobile}
-                    duration={1000}
-                    delay={500}
-                    distance="30px"
-                  >
-                    <div className="project-wrapper__text">
-                      <h3 className="project-wrapper__text-title">{name}</h3>
-                      <div>
-                        <p>{name}</p>
-                      </div>
-                    </div>
-                  </Fade>
-                </Col>
-                <Col lg={8} sm={12}>
-                  <Fade
-                    right={isDesktop}
-                    bottom={isMobile}
-                    duration={1000}
-                    delay={1000}
-                    distance="30px"
-                  >
-                    <div className="project-wrapper__text">
-                      <h3 className="project-wrapper__text-title">{version}</h3>
-                    </div>
-                    {/* <div className="project-wrapper__text">
-                      <h3 className="project-wrapper__text-title">{name}</h3>
-                    </div> */}
-                  </Fade>
-                </Col>
-              </Row>
-            );
-          })}
+        <div>
+          <Title title="Skills" />
+          <Table striped bordered hover className="project-wrapper__text-table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Version</th>
+                <th>Experience</th>
+              </tr>
+            </thead>
+            <tbody>
+              {skill.map((skillData) => {
+                const { name, id, version, experience } = skillData;
+                return (
+                  <tr key={id}>
+                    <td>{name}</td>
+                    <td>{version}</td>
+                    <td>{experience}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
         </div>
       </Container>
     </section>
